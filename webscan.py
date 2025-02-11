1847bb8dd5cd6efbdaae18d8f684c0b61107dd8d
Ivaylo Vasilev <ivaylovasilev81@gmail.com>
2025-02-11 03:18:50 +0200

    Initial commit of WebScanner

---
+++ webscan.py
@@ -0,0 +1,56 @@
+import argparse
+import sys
+import os
+import requests
+
+USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64; rv:135.0) Gecko/20100101 Firefox/135.0"
+
+parser = argparse.ArgumentParser(prog="webscan", description="web scanner for existing (and/or hidden) directories", 
+                                 epilog="(c) Ivaylo Vasilev")
+parser.add_argument("url", nargs="?", help="specify url")
+parser.add_argument("-a", "--user-agent", metavar="str", default=USER_AGENT, help="specify user agent")
+parser.add_argument("-w", "--wordlist", metavar="txt", help="specify wordlist")
+parser.add_argument("--version", action="version", version="%(prog)s 1.0.1", help="show program version")
+args = parser.parse_args()
+
+
+def main():
+    if len(sys.argv) == 1:
+        sys.exit(f"usage: {sys.argv[0]} [url] [-w wordlist]")
+    
+    if not args.url:
+        sys.exit("error: mising required argument 'url'")
+    if not args.wordlist:
+        sys.exit("error: missing required argument -w <wordlist>")
+    elif not os.path.exists(args.wordlist):
+        sys.exit(f"error: wordlist file '{args.wordlist}' does not exist")
+    
+    target_url = args.url.rstrip("/")
+
+    counter = 0
+
+    print("scanning ...")
+    with open(args.wordlist, "r") as wordlist:
+        for line in wordlist.readlines():
+            full_url = (target_url + "/" + line.strip() + "/")
+
+            response = request_dirs(full_url)
+
+            if response:
+                print(f"[+] {full_url} | code: {response.status_code}")
+                counter += 1
+    
+    if counter == 0:
+        print("nothing found.")
+
+
+def request_dirs(url):
+    user_agent = args.user_agent
+    try:
+        return requests.get(url, headers={"User-Agent": user_agent})
+    except requests.exceptions.ConnectionError:
+        pass
+
+
+if __name__ == "__main__":
+    main()
